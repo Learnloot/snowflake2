4d941a66768e3e8ed52a1e5b3e684b69e1d649ea
Learnloot <email@example.com>
2021-12-18 10:52:39 -0800

    All commits, squashed
    Remove cache files
    
    Create the repository
    
    Add to PyPi
    
    Fix package
    
    Add installation instructions
    
    Update name & package
    
    Update name & package

---
+++ src/snowflake2/snowflake.py
@@ -0,0 +1,28 @@
+# Based on https://github.com/falcondai/python-snowflake/blob/69898a84ef2b9c4919b5940120f8fc8e90723e0f/snowflake.py
+import datetime
+
+# twitter's snowflake parameters
+twepoch = 1639853814
+worker_id_bits = 5
+process_id_bits = 5
+increment_id_bits = 12
+max_worker_id = 1 << worker_id_bits
+max_process_id = 1 << process_id_bits
+max_increment_id = 1 << increment_id_bits
+max_timestamp = 1 << (64 - worker_id_bits - process_id_bits - increment_id_bits)
+
+def make_snowflake(timestamp_ms, worker_id, process_id, increment_id, twepoch=twepoch):
+    """generate a twitter-snowflake id, based on 
+    https://github.com/twitter/snowflake/blob/master/src/main/scala/com/twitter/service/snowflake/IdWorker.scala
+    :param: timestamp_ms time since UNIX epoch in milliseconds"""
+
+    sid = ((int(timestamp_ms) - twepoch) % max_timestamp) << worker_id_bits << process_id_bits << increment_id_bits
+    sid += (worker_id % max_worker_id) << process_id_bits << increment_id_bits
+    sid += (process_id % max_process_id) << increment_id_bits
+    sid += increment_id % max_increment_id
+
+    return str(sid)
+
+def snowflakeToTime(snowflake):
+    # From https://github.com/vegeta897/snow-stamp/blob/main/src/convert.js
+    return datetime.datetime.fromtimestamp(float(int(snowflake) / 4194304 + twepoch))
